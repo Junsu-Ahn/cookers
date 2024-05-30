c3c9d70dfa62a42246930629040a0f532d1b9538
PILSUNyou <dbyo1125@naver.com>
2024-05-30 15:12:58 +0900

    레시피 작성중

--- src/test/java/com/example/cookers/CookersApplicationTests.java
+++ src/test/java/com/example/cookers/CookersApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.cookers;
 
+import com.example.cookers.domain.recipe.service.RecipeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CookersApplicationTests {
 
+	@Autowired
+	RecipeService recipeService;
 	@Test
 	void contextLoads() {
+		for (int i =1; i<= 10; i++){
+			String title = String.format("제목 %d", i);
+			String content = String.format("내용 %d", i);
+			String nickname = String.format("user%d", i);
+			recipeService.create(title, content, nickname);
+		}
 	}
 
 }
