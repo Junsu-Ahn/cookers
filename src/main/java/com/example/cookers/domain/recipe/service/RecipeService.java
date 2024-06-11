fa5d1f938abcc7ed562417a654268e9fe2b6e280
PILSUNyou <156152342+PILSUNyou@users.noreply.github.com>
2024-06-11 15:31:46 +0900

    Ps recipe create (#21)
    * making to recipe create form
    
    * 메인 및 작성하기 추가
    
    * making to recipe create form
    
    * 메인 및 작성하기 추가
    
    * recipe 중간작업
    
    * recipe 중간작업

--- src/main/java/com/example/cookers/domain/recipe/service/RecipeService.java
+++ src/main/java/com/example/cookers/domain/recipe/service/RecipeService.java
@@ -3,6 +3,10 @@ package com.example.cookers.domain.recipe.service;
 import com.example.cookers.domain.recipe.entity.Recipe;
 import com.example.cookers.domain.recipe.repository.RecipeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -11,19 +15,32 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class RecipeService {
+    @Autowired
     private final RecipeRepository recipeRepository;
+
+    public Page<Recipe> getList(int page){
+        Pageable pageable = PageRequest.of(page,10);
+        return this.recipeRepository.findAll(pageable);
+    }
 
     public List<Recipe> getList() {
         return recipeRepository.findAll();
     }
 
-    public void create ( String recipe_title, String recipe_content, String nickname){
+    public void create (String title, String subject,String content, String nickname, String categoryValue){
         Recipe recipe = Recipe.builder()
-                .recipe_title(recipe_title)
-                .recipe_content(recipe_content)
+                .title(title)
+                .subject(subject)
+                .content(content)
                 .nickname(nickname)
                 .createDate(LocalDateTime.now())
+                .categoryValue(categoryValue)
                 .build();
         recipeRepository.save(recipe);
+    }
+
+    public Page<Recipe> getRecipesByCategoryValue(String categoryValue, int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return recipeRepository.findByCategoryValue(categoryValue, pageable);
     }
 }
--- src/test/java/com/example/cookers/CookersApplicationTests.java
+++ src/test/java/com/example/cookers/CookersApplicationTests.java
@@ -11,13 +11,114 @@ class CookersApplicationTests {
 	@Autowired
 	RecipeService recipeService;
 	@Test
-	void contextLoads() {
-		for (int i =1; i<= 10; i++){
+	void 카테고리별로_레시피출력() {
+		for (int i =1; i<= 1; i++){ // 1
 			String title = String.format("제목 %d", i);
+			String subject = String.format("소제목 %d", i);
 			String content = String.format("내용 %d", i);
 			String nickname = String.format("user%d", i);
-			recipeService.create(title, content, nickname);
+			String categoryValue = String.format("밥요리");
+			recipeService.create(title, subject, content, nickname, categoryValue);
 		}
+		for (int i =1; i<= 2; i++){ // 2 ~ 3
+			String title = String.format("제목 %d", i);
+			String subject = String.format("소제목 %d", i);
+			String content = String.format("내용 %d", i);
+			String nickname = String.format("user%d", i+1);
+			String categoryValue = String.format("국 & 찌개");
+			recipeService.create(title, subject, content, nickname, categoryValue);
+		}
+		for (int i =1; i<= 3; i++){ // 4 ~ 6
+			String title = String.format("제목 %d", i);
+			String subject = String.format("소제목 %d", i);
+			String content = String.format("내용 %d", i);
+			String nickname = String.format("user%d", i+3);
+			String categoryValue = String.format("면요리");
+			recipeService.create(title, subject, content, nickname, categoryValue);
+		}
+		for (int i =1; i<= 4; i++){ // 7 ~ 10
+			String title = String.format("제목 %d", i);
+			String subject = String.format("소제목 %d", i);
+			String content = String.format("내용 %d", i);
+			String nickname = String.format("user%d", i+6);
+			String categoryValue = String.format("튀김요리");
+			recipeService.create(title, subject, content, nickname, categoryValue);
+		}
+		for (int i =1; i<= 5; i++){ // 11 ~ 16
+			String title = String.format("제목 %d", i);
+			String subject = String.format("소제목 %d", i);
+			String content = String.format("내용 %d", i);
+			String nickname = String.format("user%d", i+10);
+			String categoryValue = String.format("볶음요리");
+			recipeService.create(title, subject, content, nickname, categoryValue);
+		}
+		for (int i =1; i<= 6; i++){ // 17 ~ 23
+			String title = String.format("제목 %d", i);
+			String subject = String.format("소제목 %d", i);
+			String content = String.format("내용 %d", i);
+			String nickname = String.format("user%d", i+16);
+			String categoryValue = String.format("찜 & 조림");
+			recipeService.create(title, subject, content, nickname, categoryValue);
+		}
+		for (int i =1; i<= 7; i++){ // 23 ~ 30
+			String title = String.format("제목 %d", i);
+			String subject = String.format("소제목 %d", i);
+			String content = String.format("내용 %d", i);
+			String nickname = String.format("user%d", i+22);
+			String categoryValue = String.format("샐러드");
+			recipeService.create(title, subject, content, nickname, categoryValue);
+		}
+		for (int i =1; i<= 8; i++){ // 31 ~ 39
+			String title = String.format("제목 %d", i);
+			String subject = String.format("소제목 %d", i);
+			String content = String.format("내용 %d", i);
+			String nickname = String.format("user%d", i+30);
+			String categoryValue = String.format("밑반찬");
+			recipeService.create(title, subject, content, nickname, categoryValue);
+		}
+		for (int i =1; i<= 9; i++){ // 40 ~ 49
+			String title = String.format("제목 %d", i);
+			String subject = String.format("소제목 %d", i);
+			String content = String.format("내용 %d", i);
+			String nickname = String.format("user%d", i+39);
+			String categoryValue = String.format("간식 & 분식");
+			recipeService.create(title, subject, content, nickname, categoryValue);
+		}
+		for (int i =1; i<= 10; i++){ // 50 ~ 60
+			String title = String.format("제목 %d", i);
+			String subject = String.format("소제목 %d", i);
+			String content = String.format("내용 %d", i);
+			String nickname = String.format("user%d", i+49);
+			String categoryValue = String.format("디저트");
+			recipeService.create(title, subject, content, nickname, categoryValue);
+		}
+		for (int i =1; i<= 11; i++){ // 61 ~ 72
+			String title = String.format("제목 %d", i);
+			String subject = String.format("소제목 %d", i);
+			String content = String.format("내용 %d", i);
+			String nickname = String.format("user%d", i+60);
+			String categoryValue = String.format("명절음식");
+			recipeService.create(title, subject, content, nickname, categoryValue);
+		}
+		for (int i =1; i<= 12; i++){ // 73 ~
+			String title = String.format("제목 %d", i);
+			String subject = String.format("소제목 %d", i);
+			String content = String.format("내용 %d", i);
+			String nickname = String.format("user%d", i+72);
+			String categoryValue = String.format("기타요리");
+			recipeService.create(title, subject, content, nickname, categoryValue);
+		}
+
 	}
 
+//	@Test
+//	void 카테고리_페이징() {
+//		for (int i =1; i<= 12; i++){
+//			String title = String.format("제목 %d", i);
+//			String subject = String.format("소제목 %d", i);
+//			String content = String.format("내용 %d", i);
+//			String nickname = String.format("user%d", i);
+//			recipeService.create(title, subject, content, nickname);
+//		}
+//	}
 }
